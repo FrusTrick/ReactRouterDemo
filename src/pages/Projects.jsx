f822606afd7abd2ffbfe3379477cd1f559402319
George Al Akhras <george.alakhras@hotmail.com>
2026-03-19 10:42:43 +0100

    Init project

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <p>Here are some of my projects:</p>
+      <ul>
+        <li><a href="/project1">Project 1</a></li>
+        <li><a href="/project2">Project 2</a></li>
+        <li><a href="/project3">Project 3</a></li>
+      </ul>
+    </div>
+  );
+}
